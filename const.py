81b54b4c0e5d95da84828a728daeed2ea03e3995
Luke <rugulous1999@hotmail.com>
2022-04-29 11:48:48 +0100

    Pulled secrets out to allow committing consts

---
+++ const.py
@@ -0,0 +1,10 @@
+import secrets
+
+#api
+CLIENT_ID = secrets.CLIENT_ID
+CLIENT_SECRET = secrets.CLIENT_SECRET
+TOKEN_URL = 'https://www.onlinescoutmanager.co.uk/oauth/token'
+SCOPES = ['attendance','event','programme', 'member']
+
+#section
+SECTION_NAME = 'Queensbury Scout Band'
